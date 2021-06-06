73301f0c04a0d74bcd133273e617e07abfdbb20c
Platinum139 <cay_mann@mail.ua>
2021-06-06 21:37:24 +0300

    Get form data

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"MemberClub/src/controllers"
 	"log"
 	"net/http"
 )
 
 func main() {
+	http.HandleFunc("/", controllers.HandleMembers)
 	fs := http.FileServer(http.Dir("src/static"))
-	http.Handle("/", fs)
+
+	http.Handle("/src/static/", http.StripPrefix("/src/static/", fs))
 
 	log.Println("Starting server...")
 	log.Fatalln(http.ListenAndServe(":8080", nil))
--- src/controllers/MemberController.go
+++ src/controllers/MemberController.go
@@ -1,19 +1,50 @@
 package controllers
 
 import (
+	"MemberClub/src/models"
 	template2 "html/template"
 	"log"
 	"net/http"
+	"time"
 )
 
-func HandleMembers(w http.ResponseWriter, _ *http.Request) {
-	template, err := template2.ParseFiles("src/static/index.html")
-	if err != nil {
-		log.Println(err)
-		return
+type Data struct {
+	Items []models.Member
+}
+
+var data Data
+
+func HandleMembers(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.Method, r.URL)
+
+	if r.Method == "GET" {
+		functions := template2.FuncMap{"increment": func(i int) int { return i + 1 }}
+		template := template2.Must(template2.
+				New("index.html").
+				Funcs(functions).
+				ParseFiles("src/static/index.html"))
+		err := template.Execute(w, data)
+		if err != nil {
+			log.Println(err)
+			return
+		}
 	}
-	err = template.Execute(w, nil)
-	if err != nil {
-		log.Println(err)
+	if r.Method == "POST" {
+		// get form data
+		name := r.PostFormValue("name")
+		email := r.PostFormValue("email")
+
+		// create member
+		registrationDate := time.Now()
+		newMember := models.Member{
+			Name: name,
+			Email: email,
+			RegistrationDate: registrationDate,
+		}
+		// add new member to array
+		data.Items = append(data.Items, newMember)
+
+		// redirect with method GET
+		http.Redirect(w, r, "/", http.StatusSeeOther)
 	}
 }
